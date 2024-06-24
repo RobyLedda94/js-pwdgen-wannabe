9249f0a97e73b288ff9319e985b103e9539aea80
RobyLedda94 <robyledda01@yahoo.it>
2024-06-24 15:24:10 +0200

    [UPD] add document in file js

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,6 @@
 alert("JavaScript pwdgen-wannabe");
 // ho aggiunto la funzione alert per verificare che il file javasript fosse linkato correttamente
+
+
+document.getElementById('login').innerHTML ='Inserisci Nome Cognome colore preferito e numero 23';
+// oggetto document che inserisce del testo nell'elemento h2 presente nel file index.html

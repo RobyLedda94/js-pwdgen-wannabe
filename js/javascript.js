6b70224b90dddcdd76a45ec5f1fa64b1be08ea00
RobyLedda94 <robyledda01@yahoo.it>
2024-06-24 15:53:06 +0200

    [UPD] add console in js and style classes

--- js/javascript.js
+++ js/javascript.js
@@ -2,5 +2,8 @@ alert("JavaScript pwdgen-wannabe");
 // ho aggiunto la funzione alert per verificare che il file javasript fosse linkato correttamente
 
 
-document.getElementById('login').innerHTML ='Inserisci Nome Cognome colore preferito e numero 23';
+document.getElementById('login').innerHTML ='<strong>Inserisci Nome Cognome colore preferito e numero 23</strong>';
 // oggetto document che inserisce del testo nell'elemento h2 presente nel file index.html
+
+
+console.log(document.getElementById('login'));

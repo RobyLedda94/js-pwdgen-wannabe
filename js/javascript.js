c5a8cbe813f39ba2ddaeb7c9f32b82cbb9d6b43b
RobyLedda94 <robyledda01@yahoo.it>
2024-06-24 16:38:46 +0200

    [UPD] variabili

--- js/javascript.js
+++ js/javascript.js
@@ -7,3 +7,14 @@ document.getElementById('login').innerHTML ='<strong>Inserisci Nome Cognome colo
 
 
 console.log(document.getElementById('login'));
+// con questa funzione l'elemento con id login e apparso nell'ispector voce console e mostra una serie di proprietà tra cui l'id e il contenuto inserito tramite la funzione inner html
+
+
+// Variabili 
+
+let first_name = prompt('inserisci il tuo nome');
+console.log(userName)
+
+
+let second_name = prompt('inserisci il tuo cognome');
+console.log(usersurname)
